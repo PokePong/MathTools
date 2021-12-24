495095e36875761dc31c68aeb58ab59546b927bf
Besse Loic <loic.besse@etu.enseeiht.fr>
2021-12-24 14:01:38 +0100

    InitialCommit

---
+++ src/test/java/com/loicb/mathTools/MainTest.java
@@ -0,0 +1,11 @@
+package com.loicb.mathTools;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+        System.out.println("test test");
+    }
+}
